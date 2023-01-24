8e17448b222b14a57ff2ca73a3c11826e1bde075
Tushar-435 <122785295+Tushar-435@users.noreply.github.com>
2023-01-24 17:58:05 +0530

    Add files via upload

---
+++ Check21.java
@@ -0,0 +1,41 @@
+public class Check21 {
+
+public Integer val;
+
+public void fun1() {
+
+Integer grade1 = new Integer(25);
+
+val = grade1;
+
+fun2(grade1);
+
+}
+
+private void fun2(Integer grade2) {
+
+grade2 = grade2 .intValue();
+
+if(grade2==val) {
+
+System.out.println("equal");
+
+}
+
+Else {
+
+System.out.println("not equal");
+
+}
+
+}
+
+public static void main(String ar[]) {
+
+Check21 c=new Check21();
+
+c.fun1();
+
+}
+
+}
---
+++ Simpleif.java
@@ -0,0 +1,9 @@
+class Simpleif {
+public static void main(String [] arg){
+int x = 5;
+int y = 3;
+if(x+y < 10) { // true
+System.out.println("x + y is less than 10");
+}
+}
+}
