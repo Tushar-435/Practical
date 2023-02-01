1c7e3f7e2e578af23165097a3902801de9cc2e00
Tushar-435 <122785295+Tushar-435@users.noreply.github.com>
2023-02-01 10:39:15 +0530

    Add files via upload

---
+++ Whileloop.java
@@ -0,0 +1,13 @@
+package ControlStatement;
+
+public class Whileloop {
+public static void main(String[] args) {
+	  int i=1;
+	 while(i<=10){ // condition at last
+	 System.out.println(i);
+	//while(true) {
+	//System.out.println("hello world");
+	i++;
+}
+}
+}
