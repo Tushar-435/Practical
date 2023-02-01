f1d3a1bb85995cca51f02b68872814b2ba568ef7
Tushar-435 <122785295+Tushar-435@users.noreply.github.com>
2023-02-01 10:34:07 +0530

    Add files via upload

---
+++ DoWhileloop.java
@@ -0,0 +1,15 @@
+//find fist 10 even numbers
+package ControlStatement;
+
+public class DoWhileloop {
+	public static void main(String[] args) {
+		int i = 0;
+		System.out.println("First 5 even number \n");
+		do {
+			System.out.println(i); // statement
+			i= i+2 ; // increment
+		}while(i<=10); // condition
+		}
+	}
+
+
---
+++ ForEachArr.java
@@ -0,0 +1,16 @@
+package ControlStatement;
+
+public class ForEachArr {
+	public static void main(String[] args) {
+		// declare are array
+		int arr[]= {12,22,45,25,32};
+		// using for each loop for print the array
+	 for(int i:arr) {
+		System.out.println(i);
+		}
+		//for(;;) {// infinitive (control)
+		// System.out.println("chalte raho");
+	}
+	
+// }
+}//}
