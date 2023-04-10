e7432744463c124f58df9847b0c54346bbfe708d
Tushar-435 <122785295+Tushar-435@users.noreply.github.com>
2023-04-10 14:02:34 +0530

    Add files via upload

---
+++ IO/Assigment107Apr.java
@@ -0,0 +1,20 @@
+package IO;
+
+
+	import java.util.ArrayList;
+	// method to store even numbers from 2 to N in an ArrayList and return the ArrayList
+	public class Assigment107Apr {
+	public static ArrayList<Integer> storeEvenNumbers(int N) {
+	// create an empty ArrayList to store even numbers
+	ArrayList<Integer>A1 = new ArrayList<>();
+	// Use For loop from 2 to N 
+	for (int i = 2; i <= N; i += 2) {
+	A1.add(i); 
+	}
+	return A1; // return the ArrayList containing A1 from 2 to N
+	}
+	public static void main(String[] args) { 	// main method 
+	ArrayList<Integer> E = storeEvenNumbers(30);
+	System.out.println(E);// print the result of  ArrayList 
+	}}
+
